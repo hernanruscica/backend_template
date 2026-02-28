951b2cab70bf7e351a293b74a924b96756399652
hernanruscica <cesarhernanruscica@gmail.com>
2026-02-28 11:34:39 -0300

    backup

--- src/jobs/alarmJob.js
+++ src/jobs/alarmJob.js
@@ -5,7 +5,7 @@ let isRunning = false;
 
 const startAlarmJob = () => {
   // Ejecutar cada 5 minutos: '*/5 * * * *' o 30 segundos: '*/30 * * * * *'
-  cron.schedule('*/30 * * * * *', async () => {
+  cron.schedule('*/5 * * * *', async () => {
     if (isRunning) {
       console.log('⚠️ El job anterior de alarmas sigue corriendo. Saltando esta ejecución.');
       return;
--- src/models/UserAlarmModel.js
+++ src/models/UserAlarmModel.js
@@ -13,7 +13,7 @@ const findUsersByAlarmUuid = async (alarmUuid) => {
     console.log('Buscando usuarios asociados con la alarma con uuid: ', alarmUuid); 
     //SELECT * FROM `users_alarms` WHERE alarm_uuid = 'a1b2c3d4-0001-4a7b-8c9d-0e1f2a3b4c5d'; 
     const query = `
-        SELECT users_alarms.alarm_uuid, users_alarms.user_uuid, users_alarms.business_uuid,
+        SELECT users_alarms.uuid as user_alarm_uuid, users_alarms.alarm_uuid, users_alarms.user_uuid, users_alarms.business_uuid,
         users.first_name, users.last_name, users.dni, users.email, users.phone 
         FROM ${UserAlarmModel.tableName} 
         INNER JOIN users ON users_alarms.user_uuid = users.uuid
